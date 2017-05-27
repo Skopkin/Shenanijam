9a6e9dac3c39ef5550cd4586d0e19f120f4082ff
Skopkin <skopkin@aol.com>
2017-05-26 23:28:27 -0400

    waves

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,18 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour {
 
-	public float speed;
+	public float speed, minX, minY, maxX, maxY;
 	private Rigidbody2D rb;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update() {
+		Vector2 pos = rb.position;
+		pos.x = Mathf.Clamp (pos.x, minX, maxX);
+		pos.y = Mathf.Clamp (pos.y, minY, maxY);
+		rb.position = pos;
 	}
 
 	void FixedUpdate () 
---
+++ Assets/Scripts/Surfer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Surfer : MonoBehaviour {
+
+	public float speed;
+	private float direction;
+	private float force;
+	private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		if (rb.position.x > 0)
+			direction = -1;
+		else
+			direction = 1;
+		
+		force = speed * direction;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.AddForce(new Vector2(force, 0.0f));
+	}
+}
