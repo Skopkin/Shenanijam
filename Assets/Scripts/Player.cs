d4ce4a7316a37bf29e805b53ce43acdae7ca1207
John Chapman <j.p.chapman1@gmail.com>
2017-05-26 23:35:38 -0400

    commit
    me up fam

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,19 +7,25 @@ public class Player : MonoBehaviour {
 
 	public float speed;
 	private Rigidbody2D rb;
+	private SpriteRenderer spriteRen;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
+		spriteRen = GetComponent<SpriteRenderer>();
 	}
 
-	void FixedUpdate () 
-	{
+	void FixedUpdate () {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 
 		rb.AddForce (movement * speed);
+		if (movement.x < 0) {
+			spriteRen.flipX = true;
+		} else {
+			spriteRen.flipX = false;
+		}
 
 		if (Input.GetKeyDown (KeyCode.Space)) {
 			spinToWin ();
